256aeb7fdbea40005bf0a79da2d2d97c5551032a
Cyril26 <cyrilodm26@gmail.com>
2024-11-29 15:14:57 +0000

    add link

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,4 +1,4 @@
-"uce client";
+"use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
 import { projects } from "@/data";
@@ -17,10 +17,7 @@ const RecentProjects = () => {
             className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
             key={item.id}
           >
-            <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
-            >
+            <PinContainer title={item.title} href={item.link}>
               <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
                 <div
                   className="relative w-full h-full overflow-hidden lg:rounded-3xl"
@@ -66,7 +63,9 @@ const RecentProjects = () => {
 
                 <div className="flex justify-center items-center">
                   <p className="flex lg:text-xl md:text-xs text-sm text-purple">
-                    Check Live Site
+                    <a href={item.link} target="blank">
+                      Check Live Site
+                    </a>
                   </p>
                   <FaLocationArrow className="ms-3" color="#CBACF9" />
                 </div>
